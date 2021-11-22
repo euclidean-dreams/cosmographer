374e5fc3d2c876606c7e9b97c35b53d34e13c401
Josiah Chapman <josiah.chapman@gmail.com>
2021-11-22 14:33:05 -0800

    removed build folder

--- cmake-build-debug/libsocket-build/C++/CMakeFiles/socket++_int.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libsocket++.a"
-  "libsocket++.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/socket++_int.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- cmake-build-debug/yaml-build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/josiah/projects/code/impresario-systems/cosmographer/cmake-build-debug/yaml-src
-# Build directory: /home/josiah/projects/code/impresario-systems/cosmographer/cmake-build-debug/yaml-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
-subdirs("util")
