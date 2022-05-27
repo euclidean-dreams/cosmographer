26aa64dc1471982057ea6696bf76ce0d0a5a4788
josiahdc <josiah.chapman@gmail.com>
2022-05-27 15:57:54 -0700

    volitia is back in the driver's seat

--- src/cloister/VolitiaGolem.cpp
+++ src/cloister/VolitiaGolem.cpp
@@ -12,26 +12,26 @@ VolitiaGolem::VolitiaGolem(
 }
 
 void VolitiaGolem::refresh() {
-    if (community->lumionBookie->activationCount > 0) {
-        averageNonZeroLumionActivationCount.addSample(community->lumionBookie->activationCount);
-    }
-
-    for (int index = 0; index < paradigm->axioms.size(); index++) {
-        if (index == 6) {
-            auto delta = community->lumionBookie->activationCount / averageNonZeroLumionActivationCount.value - 1;
-            paradigm->axioms[index] = Tidbit::bind(paradigm->axioms[index] + 0.1 * delta, 0.1f, 0.9f);
-        } else if (index != 1 && index != 2) {
-            auto deltaMax = 0.01 * community->lumionBookie->activationCount / 50;
-            auto delta = community->randomizer->generateProportion() * deltaMax - deltaMax / 2;
-            paradigm->axioms[index] = Tidbit::bind(paradigm->axioms[index] + delta, 0.1f, 0.9f);
-        }
-    }
-    if (community->lumionBookie->activationCount > averageNonZeroLumionActivationCount.value * 5) {
-        paradigm->microMode = CLOISTER->randomizer->generate(5);
-    }
-    if (community->lumionBookie->activationCount > averageNonZeroLumionActivationCount.value * 10) {
-        paradigm->macroMode = CLOISTER->randomizer->generate(5);
-    }
+//    if (community->lumionBookie->activationCount > 0) {
+//        averageNonZeroLumionActivationCount.addSample(community->lumionBookie->activationCount);
+//    }
+//
+//    for (int index = 0; index < paradigm->axioms.size(); index++) {
+//        if (index == 6) {
+//            auto delta = community->lumionBookie->activationCount / averageNonZeroLumionActivationCount.value - 1;
+//            paradigm->axioms[index] = Tidbit::bind(paradigm->axioms[index] + 0.1 * delta, 0.1f, 0.9f);
+//        } else if (index != 1 && index != 2) {
+//            auto deltaMax = 0.01 * community->lumionBookie->activationCount / 50;
+//            auto delta = community->randomizer->generateProportion() * deltaMax - deltaMax / 2;
+//            paradigm->axioms[index] = Tidbit::bind(paradigm->axioms[index] + delta, 0.1f, 0.9f);
+//        }
+//    }
+//    if (community->lumionBookie->activationCount > averageNonZeroLumionActivationCount.value * 5) {
+//        paradigm->microMode = CLOISTER->randomizer->generate(5);
+//    }
+//    if (community->lumionBookie->activationCount > averageNonZeroLumionActivationCount.value * 10) {
+//        paradigm->macroMode = CLOISTER->randomizer->generate(5);
+//    }
 }
 
 }
